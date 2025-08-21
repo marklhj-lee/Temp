de7d4bb2fc9a0eeec688a6b8e885f1582d97ec49
Mark Lee <marklhj@gmail.com>
2025-08-21 13:05:17 -0400

    Edit the way async works

--- temp.py
+++ temp.py
@@ -1,30 +1,27 @@
-def save_to_db(data):
-    async def _save():
-        async with get_db() as db:
-            res = await db.execute(select(Config).limit(1))
-            existing_config = res.scalars().first()
-            if not existing_config:
-                new_config = Config(data=data, version=0)
-                db.add(new_config)
-            else:
-                existing_config.data = data
-                existing_config.updated_at = datetime.now()
-                db.add(existing_config)
-            await db.commit()
-    return asyncio.run(_save())
+import asyncio
+CONFIG_DATA: dict = asyncio.run(get_config())
+
+async def save_to_db(data: dict) -> None:
+    async with get_db() as db:
+        res = await db.execute(select(Config).limit(1))
+        existing_config = res.scalars().first()
+        if not existing_config:
+            db.add(Config(data=data, version=0))
+        else:
+            existing_config.data = data
+            existing_config.updated_at = datetime.now()
+            db.add(existing_config)
+        await db.commit()
 
 
-def reset_config():
-    async def _reset():
-        async with get_db() as db:
-            await db.execute(delete(Config))
-            await db.commit()
-    return asyncio.run(_reset())
+async def reset_config() -> None:
+    async with get_db() as db:
+        await db.execute(delete(Config))
+        await db.commit()
 
-def get_config():
-    async def _get():
-        async with get_db() as db:
-            res = await db.execute(select(Config).order_by(Config.id.desc()).limit(1))
-            config_entry = res.scalars().first()
-            return config_entry.data if config_entry else DEFAULT_CONFIG
-    return asyncio.run(_get())
+
+async def get_config() -> dict:
+    async with get_db() as db:
+        res = await db.execute(select(Config).order_by(Config.id.desc()).limit(1))
+        config_entry = res.scalars().first()
+        return config_entry.data if config_entry else DEFAULT_CONFIG
